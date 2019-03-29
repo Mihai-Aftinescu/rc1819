2993f74e739d0bd4a38e50775f566b44ae55812e
unknown <mihaiionut.aftinescu961@alu.uhu.es>
2019-03-29 09:41:49 +0100

    EntreXY mejorado

--- primosEntreXY.pl
+++ primosEntreXY.pl
@@ -8,12 +8,18 @@
 
 primosEntreXY(X,Y, []):- X > Y.
 
-primosEntreXY(X,Y,[X|R]):- X =< Y, X2 is X + 1, primo(X),
-	primosEntreXY(X2, Y, R).
+primosEntreXY(X,Y,[X|R]):- 
+		X =< Y,
+		X2 is X + 1, 
+		primo(X),
+		primosEntreXY(X2, Y, R).
 
 	
-primosEntreXY(X,Y,R):- X =< Y,X2 is X + 1, \+ primo(X),
-	primosEntreXY(X2, Y, R).
+primosEntreXY(X,Y,R):- 
+		X =< Y,
+		X2 is X + 1,
+		\+ primo(X),
+		primosEntreXY(X2, Y, R).
 	
 
 
@@ -38,7 +44,18 @@ divisores(X, R) :- divisores(X,X,R).
 % desde 1 hasta Y.
 
 
-divisores(_,1,[1]).
+divisores(X,1,[1]):- number(X).
 
-divisores(X,Y,[Y|R]):-  Y > 1, Y2 is Y - 1,  divisores(X,Y2,R), 0 is X mod Y.
-divisores(X,Y,R):-  Y > 1, Y2 is Y - 1,  divisores(X,Y2,R), Resto is X mod Y, Resto \=0.
+divisores(X,Y,[Y|R]):-  
+		Y > 1,
+		Y2 is Y - 1,  
+		divisores(X,Y2,R), 
+		0 is X mod Y.
+		
+divisores(X,Y,R):-  
+		Y > 1, 
+		Y2 is Y - 1,  
+		divisores(X,Y2,R), 
+		Resto is X mod Y, 
+		Resto \=0.
+
