7feeac22de6ffd56360d9ae4f8ae4b24d016fcd6
unknown <mihaiionut.aftinescu961@alu.uhu.es>
2019-04-02 13:56:19 +0200

    Peano 2.0

--- Aritmetica_Peano.pl
+++ Aritmetica_Peano.pl
@@ -41,7 +41,7 @@ producto(n(X), Y, Z2 ):-
 
 p2d(0,0).
 
-p2d(n(X), R):- p2d(X, D), R is D + 1;
+p2d(n(X), R):- nonvar(X), p2d(X, D), R is D + 1.
 
 
 % d2p(+D, - P)
@@ -49,4 +49,62 @@ p2d(n(X), R):- p2d(X, D), R is D + 1;
 % El numero en aritmética de Peano
 % representación equivalente a su
 
-d2p(X, n(P)):- X2 is X - 1, d2p(X2,P).
+d2p(0,0).
+d2p(X, n(P)):- X >= 1, X2 is X - 1, d2p(X2,P).
+
+
+
+
+% division(?X, ?Y, ?Z)
+% es cierto si Z es un resultado de la division
+% entera de X entre Y
+
+  
+ division(X, Y, 0):- menor(X, Y).
+ 
+ division(X, Y, n(Z)):- resta(X, Y, R), division(R, Y, Z).
+  
+
+% menor(?X, ?Y)
+% es cierto si X es menor estricto que Y
+
+menor(0, n(_)).
+menor(n(X), n(Y)):- menor(X,Y).
+
+
+
+% modulo(?X. ?Y, ?Z)
+% es cierto si Z unifica con el resto de la division
+% entera de X entre Y
+
+modulo(X,X,0).
+
+modulo(X, Y, Tot):- division(X, Y, R), producto(R, Y, Res), resta(X, Res, Tot).
+
+
+
+% mayor_o_igual(?X, ?Y).
+% es cierto si X es mayor o igual que Y
+
+mayor_o_igual(0,0).
+mayor_o_igual(n(X), 0):- peano(X).
+mayor_o_igual(n(X), n(Y):- mayor_o_igual(X,Y).
+
+
+
+
+% peano(?X)
+% es cierto si X es un numero valido en la representación de la Aritmética de Peano
+
+peano(0).
+
+peano(n(X)):- peano(X).
+
+
+
+
+
+
+
+
+
